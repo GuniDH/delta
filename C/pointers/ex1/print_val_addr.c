60c0f29951d66260d0149f0147541056cec095dc
GuniDH <114918509+GuniDH@users.noreply.github.com>
2025-11-18 15:20:33 +0200

    fix to 1

--- C/pointers/ex1/print_val_addr.c
+++ C/pointers/ex1/print_val_addr.c
@@ -10,17 +10,28 @@
 int main() {
     int nums[LEN];
     char chars[LEN];
-    printf("give array of nums");
+    printf("give array of %d nums", LEN);
     for(int i = 0; i<LEN;i++){
-        scanf(" %d",&nums[i]);
+        if(scanf(" %d",&nums[i])!=1){
+            printf("Bad input!");
+            return 1; // We werent told whether we should give another opportunity to the user or not
+            // also let me know if you prefer that ill print errors into stderr
+        }
     }
-    printf("give array of chars");
+    printf("give array of %d chars", LEN);
     for(int i = 0; i<LEN;i++){
-        scanf(" %c",&chars[i]);
+        if(scanf(" %c",&chars[i])!=1){
+            printf("Bad input!");
+            return 1;
+        }
     }
     for(int i = 0; i<LEN;i++){
-        printf("\nFIRST WAY\narray of chars\nval: %c addr: %p\narray of nums\nval: %d addr: %p\n", chars[i], &chars[i], nums[i], &nums[i]);
-        printf("\nSECOND WAY\narray of chars\nval: %c addr: %p\narray of nums\nval: %d addr: %p\n", *(chars+i), chars+i, *(nums+i), nums+i);
+        printf("first way\n");
+        printf("array of chars\nval: %c addr: %p\n", chars[i], &chars[i]);
+        printf("array of nums\nval: %c addr: %p\n", nums[i], &nums[i]);
+        printf("second way\n");
+        printf("array of chars\nval: %c addr: %p\n",  *(chars+i), chars+i);
+        printf("array of nums\nval: %c addr: %p\n", *(nums+i), nums+i);
     }
     return 0;
 }
