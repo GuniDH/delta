26ea444f8fd43a5c9ff0bb4aa9682fb676b128d5
GuniDH <114918509+GuniDH@users.noreply.github.com>
2025-11-21 21:42:45 +0200

    sort

--- C/security/sort/sort.c
+++ C/security/sort/sort.c
@@ -8,59 +8,73 @@
 #include <string.h>
 
 #define NUM_STRINGS (5)
+#define INITIAL_SIZE 64
+#define MAX_UINT 0xFFFFFFFF
 
+// a safe implementation for scanf using dynamic allocation
 char* dyn_scanf(void) {
-    char* res = NULL;
-    int length = 0; // length of res
-    char c;
+    int size = INITIAL_SIZE;
+    unsigned int length = 0;
+    char* res = (char*)malloc(size);
+    if (!res) return NULL;
+    int c; // to handle EOF if we are given input from file
     while (1) {
         c = fgetc(stdin);
-        char* tmp = (char*)realloc(res, sizeof(char) * (++length)); // we use a temp in order to be able to free our allocated memory in case of failure
-        if (tmp == NULL) {
-            printf("Memo allocation failed. get yourself a new ram dear friend\n");
-            free(res);
-            return NULL;
-        }
-        res = tmp;
-        if (c == '\n' || c==EOF) { //this way our function is suitable both for normal stdin and also if the user gives input as file and doenst place \n
-            res[length - 1] = '\0';
+        if (c == '\n' || c == EOF) { //this way our function is suitable both for normal stdin and also if the user gives input as file and doenst place \n
+            res[length] = '\0';
             return res;
         }
-        res[length - 1] = c;
+        if (length + 1 >= size) { // check if we need to resize
+            size *= 2;
+            if (size > MAX_UINT)
+                size = MAX_UINT;
+            char* tmp = (char*)realloc(res, size);
+            if (tmp == NULL) {
+                printf("Memo allocation failed. get yourself a new ram dear friend\n");
+                free(res);
+                return NULL;
+            }
+            res = tmp;
+        }
+        res[length] = (char)c; // because fgetc returns int and we need to handle EOF
+        length++;
     }
-    return NULL;
 }
 
-int compare(void *n1, void* n2){
-    n1 = (double*)n1;
-    n2 = (double*)n2;
 
-}
-
-void sort(void* arr, int len, void (*compare)(void*, void*)){
+void sort(void** arr, int len, int (*compare)(void*, void*)){
+    void*tmp;
     for(int i =0;i<len;i++){
         for(int j=i+1;j<len;j++){
-           if(compare(arr[i], arr[j])){
-                
+           if(compare(arr[i], arr[j]) > 0){
+                tmp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=tmp;
            }
         }
     }
 }
 
-int cmp_len(char* s1, char*s2){
-    return (strlen(s1) > strlen(s2));
+int cmp_len(void* s1, void* s2) {
+    char* str1 = (char*)s1;
+    char* str2 = (char*)s2;
+    // returns postive if str1 longer, negative if 2 and 0 if equale
+    return (int)(strlen(str1) - strlen(str2));
 }
 
-int cmp_alpha(char*s1, char*s2){
-    int l1 = strlen(s1), l2=strlen(s2);
-    int min_len=(l1<l2)?l1:l2;
-    for(int i =0;i<min_len;){
-        while(s1[i]==s2[i]){i++;}
-        if (s1[i]>s2[i]){
-            return 1;
+int cmp_alpha(void* s1, void* s2) {
+    char* str1 = (char*)s1;
+    char* str2 = (char*)s2;
+    int i = 0;
+    while (str1[i] != '\0' && str2[i] != '\0') {
+        if (str1[i] != str2[i]) {
+            // asscii difference
+            return str1[i] - str2[i];
         }
+        i++;
     }
-    return 0;
+    // we finished scnanning the shorter one
+    return str1[i] - str2[i];
 }
 
 void print_strings(char**strings){
@@ -69,17 +83,31 @@ void print_strings(char**strings){
         if (i!=NUM_STRINGS-1)
             putchar(',');
     }
+    putchar('\n');
 }
 
 int main() {
-    char**strings;
-    for (int i = 0; i < NUM_STRINGS; i++){
-        printf("Enter %d string: ", i+1);
-        strings[i]=dyn_scanf();
-        putchar('\n');
+    char* strings[NUM_STRINGS]; 
+    // get input (strings)
+    for (int i = 0; i < NUM_STRINGS; i++) {
+        printf("Enter string #%d: ", i + 1);
+        strings[i] = dyn_scanf();
+        if (strings[i] == NULL) {
+            printf("Error reading input.\n");
+            return 1;
+        }
     }
+    // sort by length
+    sort((void**)strings, NUM_STRINGS, cmp_len);
     print_strings(strings);
-    sort(strings, NUM_STRINGS, cmp_alpha);
+
+    // sort by abc
+    sort((void**)strings, NUM_STRINGS, cmp_alpha);
     print_strings(strings);
-    return 1;
+
+    for (int i = 0; i < NUM_STRINGS; i++) {
+        free(strings[i]);
+    }
+
+    return 0;
 }
